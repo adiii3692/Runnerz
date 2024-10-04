8c90e4c213112d91126649e25f92e37d53491924
Aditya Nair <adinair0206@gmail.com>
2024-10-04 11:19:10 -0400

    Did some logging

--- src/main/java/com/example/runnerz/WelcomeMessage.java
+++ src/main/java/com/example/runnerz/WelcomeMessage.java
@@ -1,5 +1,4 @@
-package foo.bar;
-
+package com.example.runnerz;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/example/runnerz/app.java
+++ src/main/java/com/example/runnerz/app.java
@@ -1,18 +1,22 @@
 package com.example.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import foo.bar.WelcomeMessage;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class app {
 
+	private static final Logger log = LoggerFactory.getLogger(org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application.class);
 	public static void main(String[] args) {
-		SpringApplication.run(app.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(app.class, args);
 
-		var welcomeMessage = new WelcomeMessage();
+		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
 		System.out.println(welcomeMessage.getWelcomeMessage());
+
+		log.info("Checking if logs work: ");
 	}
 
 }
